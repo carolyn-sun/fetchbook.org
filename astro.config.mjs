1ec1c0d5e2082c1ef40cda3260ae38e54d8aacf0
Carolyn Sun <117613176+carolyn-sun@users.noreply.github.com>
2026-03-25 02:50:29 +0800

    style: fix code block

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,9 @@ import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   output: 'server',
+  server: {
+    port: 8787,
+  },
   adapter: cloudflare({
     platformProxy: {
       enabled: true,
